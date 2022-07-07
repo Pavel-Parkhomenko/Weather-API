c8008c85a0efb5e925db5d8dd9fdd17a17de5d66
Pavel Parkhomenka <pawelparhomenko@gmail.com>
2022-07-07 18:35:56 +0300

    feat: init project

---
+++ src/App/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+// import { ThemeProvider } from 'styled-components'
+// import { GlobalStyle } from '@/globalStyle'
+
+export function App() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=94a02e927f7a6862591f4816ff0872f9')
+  //       const data = await response.json()
+  //       console.log('data')
+  //       console.log(data)
+  //     } catch (err) {
+  //       console.log(err)
+  //     }
+  //   }
+  //   fetchData()
+  // }, [])
+  return (
+    <div className="App">
+      App
+    </div>
+  )
+}
